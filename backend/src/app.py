911c9675516bf37fe32bb5590d379ff3516ef8cf
Jian An Chew <jianan@Jians-Mac-mini.local>
2025-09-14 15:50:40 +0800

    first commit

---
+++ backend/src/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .routes import challenge, webhooks
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # allow anyone to send request to backend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+app.include_router(challenge.router, prefix="/api")
+app.include_router(webhooks.router, prefix="/webhooks")
